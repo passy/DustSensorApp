6863ae40e90e06c00030989e9631666e7068c923
Pascal Hartig <phartig@rdrei.net>
2017-11-13 22:47:10 +0000

    Refactor sensor data adapter

--- app/src/main/java/com/wegrzyn/marcin/dustsensorapp/SensorDataAdapter.kt
+++ app/src/main/java/com/wegrzyn/marcin/dustsensorapp/SensorDataAdapter.kt
@@ -12,20 +12,15 @@ class SensorDataAdapter(context: Context, objects: List<SensorData>) : ArrayAdap
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_layout, parent, false)
-        val dateTextView = view.findViewById<TextView>(R.id.Data)
-        val pm2TextView = view.findViewById<TextView>(R.id.PM2)
-        val pm10TextView = view.findViewById<TextView>(R.id.PM10)
-
+        val textDate: TextView = view.findViewById(R.id.text_date)
+        val textPm25: TextView = view.findViewById(R.id.text_pm25)
+        val textPm10: TextView = view.findViewById(R.id.text_pm25)
         val sensorData = getItem(position)
 
-        val datestring = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(sensorData!!.date)
+        textDate.text = SimpleDateFormat.getDateTimeInstance().format(sensorData.date)
+        textPm25.text = sensorData.pm25.toString()
+        textPm10.text = sensorData.pm10.toString()
 
-        dateTextView.text = datestring
-        val pm2 = sensorData.pm25
-        val pm10 = sensorData.pm10
-        pm2TextView.text = pm2.toString()
-        pm10TextView.text = pm10.toString()
         return view
     }
-
 }
