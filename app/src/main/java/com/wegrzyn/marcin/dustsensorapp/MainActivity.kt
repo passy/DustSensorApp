029b7f99290318810abc4e92c083bf594ed7194c
Pascal Hartig <phartig@rdrei.net>
2017-11-14 22:07:58 +0000

    Remove silly menu

--- app/src/main/java/com/wegrzyn/marcin/dustsensorapp/MainActivity.kt
+++ app/src/main/java/com/wegrzyn/marcin/dustsensorapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.wegrzyn.marcin.dustsensorapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import com.google.firebase.database.ChildEventListener
@@ -37,19 +35,19 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
-                Log.d(Tag, "onChildChange")
+                Log.d(TAG, "onChildChange")
             }
 
             override fun onChildRemoved(dataSnapshot: DataSnapshot) {
-                Log.d(Tag, "onChildRemoved")
+                Log.d(TAG, "onChildRemoved")
             }
 
             override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {
-                Log.d(Tag, "onChildMoved")
+                Log.d(TAG, "onChildMoved")
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Log.d(Tag, "onCanceled")
+                Log.d(TAG, "onCanceled")
             }
         })
 
@@ -64,31 +62,8 @@ class MainActivity : AppCompatActivity() {
         progress_list.visibility = View.INVISIBLE
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        if (id == R.id.exit) {
-            finish()
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
     companion object {
-
-        private val Tag = MainActivity::class.java.simpleName
-
+        private val TAG = MainActivity::class.java.simpleName
         private val SENSOR_DATA = "SensorData"
     }
 }
